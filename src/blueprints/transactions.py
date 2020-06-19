159f1852512941f3ebfc5bf8040c4848ecd56d24
Nikita Voronov <vedro199513@gmail.com>
2020-06-19 20:36:47 +0500

    Добалены соответствующий код для DELETE /transactions/<id>, подправлен декоратор, добавлены изменения с гитхаба

--- src/blueprints/transactions.py
+++ src/blueprints/transactions.py
@@ -10,7 +10,9 @@ from services.transactions import (
     NegativeValue,
     CategoryNotExists,
     OtherUserCategory,
-    TransactionAddingFailedError
+    TransactionAddingFailedError,
+    OtherUserTransaction,
+    TransactionNotExists
 )
 from services.decorators import auth_required
 
@@ -46,4 +48,32 @@ class TransactionsView(MethodView):
                 return new_transaction, 201
 
 
+class TransactionDeleteView(MethodView):
+    @auth_required
+    def delete(self, user, transaction_id):
+        """
+        Обработчик DELETE-запроса на удаление существующей операции
+        :param user: идентификатор авторизованного пользователя
+        :param transaction_id: идентификатор удаляемой операции
+        :return:
+        """
+        data_to_delete = {
+            'user_id': user['id'],
+            'transaction_id': transaction_id
+        }
+
+        with db.connection as connection:
+            service = TransactionsService(connection)
+
+            try:
+                deleted_transaction = service.delete_transaction(data_to_delete)
+            except OtherUserTransaction:
+                return '', 403
+            except TransactionNotExists:
+                return '', 404
+            else:
+                return deleted_transaction, 200
+
+
 bp.add_url_rule('', view_func=TransactionsView.as_view('transactions'))
+bp.add_url_rule('/<int:transaction_id>', view_func=TransactionDeleteView.as_view('transaction'))
